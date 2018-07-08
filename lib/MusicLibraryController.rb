da194d568a843308e988fc2c91c6d4bbbe35911b
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-08 02:53:31 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -45,7 +45,9 @@ class MusicLibraryController
     puts "Please enter the name of an artist:"
     artist_name = gets
     artist = Artist.find_by_name(artist_name)
-    artist.songs.sort_by!{|song| song.name}
-    artist.songs.each{|song| puts "#{song.name}"}
+    if artist
+      artist.songs.sort_by!{|song| song.name}
+      artist.songs.each{|song| puts "#{song.name}"}
+    end
   end
 end
